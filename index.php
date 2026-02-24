3a57f1bec65e8845b89abf18ebd29d331db8ef6d
unknown <isaac.sousa77@aluno.ce.gov.br>
2026-02-24 08:21:47 -0300

    aula 24/02

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Recursos Humanos</title>
+</head>
+<body>
+    <h2></h2>
+    <?php
+    class funcionario{
+        protected $nome;
+        protected $salario_base;
+    }
+    public function __construct($nome, $salario_base){
+    }
+
+    ?>
+</body>
+</html>
